8759c5188da03b730369af11c5e096c9745fadce
Sam Hopper <samh@englishlanguageitutoring.com>
2023-02-22 13:00:46 +0000

    Day 1, part 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,6 +1,8 @@
-(ns advent-of-code.core)
+(ns advent-of-code.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn day1-1
+  "https://adventofcode.com/2022/day/1"
+  []
+  (let [elves (map (fn [x] (map #(Integer/parseInt %) x)) (remove #(= '("") %) (partition-by #(= "" %) (str/split-lines (slurp "input_day1.txt")))))]
+    (first (sort > (map #(reduce + %) elves)))))
