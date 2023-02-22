0dea3456c3d7a77e76385aee0b23090a2623282a
Sam Hopper <samh@englishlanguageitutoring.com>
2023-02-22 15:12:12 +0000

    Day 3 part 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,8 +1,9 @@
 (ns advent-of-code.core
   (:require [advent-of-code.day1 :as day1] 
-            [advent-of-code.day2 :as day2]))
+            [advent-of-code.day2 :as day2]
+            [advent-of-code.day3 :as day3]))
 
 (defn -main 
   []
-  (println (day2/part2)))
+  (println (day3/part1)))
 
---
+++ src/advent_of_code/day3.clj
@@ -0,0 +1,28 @@
+(ns advent-of-code.day3
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
+
+(defn- load-input
+  [] 
+  (str/split (slurp "input_day3.txt") #"\n"))
+
+(defn- is-upper-case? 
+  [c] 
+  (and (> (int c) 65) (< (int c) 91)))
+
+(defn- priority 
+  [c]
+  (if (is-upper-case? c)
+    (- (int c) 38)
+    (- (int c) 96)))
+
+(defn- find-error-items
+  [rucksack]
+  (set/intersection
+   (set (subs rucksack 0 (/ (.length rucksack) 2)))
+   (set (subs rucksack (/ (.length rucksack) 2)))))
+
+(defn part1 
+  "https://adventofcode.com/2022/day/3"
+  []
+  (reduce + (flatten (map (fn [x] (map priority (find-error-items x))) (load-input)))))
